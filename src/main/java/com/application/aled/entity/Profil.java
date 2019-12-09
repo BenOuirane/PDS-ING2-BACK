e22309cc8cebab3e9de053536983ce18df3a11b4
Raymond Keita <raymondkeita1@gmail.com>
2019-12-09 18:15:15 +0100

    Create Profil and TypeProfil

---
+++ src/main/java/com/application/aled/entity/Profil.java
@@ -0,0 +1,65 @@
+package com.application.aled.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Profil {
+	
+	@Id
+	private long id;
+	
+	@Column(name = "firstname")
+	private String name;
+	
+	private TypeProfil typeProfil;
+	
+	@Column(name = "profil")
+	private List<TypeProfil> listTypeProfil;
+
+	
+	public Profil(long id, String name, TypeProfil typeProfil, List<TypeProfil> listTypeProfil) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.typeProfil = typeProfil;
+		this.listTypeProfil = listTypeProfil;
+	}
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public TypeProfil getTypeProfil() {
+		return typeProfil;
+	}
+
+	public void setTypeProfil(TypeProfil typeProfil) {
+		this.typeProfil = typeProfil;
+	}
+
+	public List<TypeProfil> getListTypeProfil() {
+		return listTypeProfil;
+	}
+
+	public void setListTypeProfil(List<TypeProfil> listTypeProfil) {
+		this.listTypeProfil = listTypeProfil;
+	}
+	
+}
