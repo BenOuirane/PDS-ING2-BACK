2dda4e155445b8e3d613275c89c505de98cb3b53
JAguesse <jaguesse@outlook.fr>
2020-02-10 10:22:10 +0100

    annotation update on repo

--- src/main/java/com/application/aled/repository/RoomRepository.java
+++ src/main/java/com/application/aled/repository/RoomRepository.java
@@ -2,7 +2,9 @@ package com.application.aled.repository;
 
 import com.application.aled.entity.Rooms;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Rooms,Long> {
 
 }
