bfd12de2289ac459db81bf1f48f5471c5d85a2c6
LucasMeunier <lucas.meunier@etu.u-pec.fr>
2020-02-21 15:08:58 +0100

    Vérifier d'un objet envoie des messages 0.52
    Implement waitingChecker and test and it. (not working )

--- src/main/java/com/application/aled/messages/WaitingTimeChecker.java
+++ src/main/java/com/application/aled/messages/WaitingTimeChecker.java
@@ -1,25 +1,28 @@
 package com.application.aled.messages;
 
-import com.application.aled.entity.Message;
+import com.application.aled.entity.Failure;
 import com.application.aled.entity.Objects;
-import com.application.aled.repository.LampRepository;
+import com.application.aled.entity.history.*;
 import com.application.aled.repository.MessageRepository;
+import com.application.aled.repository.UserRepository;
+import com.application.aled.service.FailureService;
 import com.application.aled.service.MessageServiceImpl;
 import com.application.aled.service.ObjectService;
+import com.application.aled.service.UserService;
+import com.application.aled.service.history.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-@Configuration
+@Service
 public class WaitingTimeChecker {
     @Autowired
     MessageRepository messageRepository;
@@ -27,57 +30,137 @@ public class WaitingTimeChecker {
     MessageServiceImpl messageService;
     @Autowired
     ObjectService objectService;
+    @Autowired
+    FailureService failureService;
+    @Autowired
+    LampHistoryService lampHistoryService;
+    @Autowired
+    OvenHistoryService ovenHistoryService;
+    @Autowired
+    CoffeeMachineHistoryService coffeeMachineHistoryService;
+    @Autowired
+    ShutterHistoryService shutterHistoryService;
+    @Autowired
+    AlarmClockHistoryService alarmClockHistoryService;
+    //Test de mort
+    @Autowired
+    UserRepository userRepository;
+
     /*@Autowired
     LampRepository lampRepository;*/
     Logger logger = Logger.getLogger("com.application.aled.messages.WaitingTimeChecker");
 
-    @PostConstruct
-    @Transactional
+    //@Async("threadPoolTaskExecutor")
     public void launchVerification(){
+        while (true) {
+            try {
+                Thread.sleep(10000);
+                logger.info("System starting check time for all objects ");
 
-        try {
-            Thread.sleep(6000);
-            logger.info("System starting check time for all objects ");
+                //List<Message> messageList = messageService.getMessages();
+                List<Objects> objectsList = objectService.getObjectByState(true);
+                for (Objects objectToCheck : objectsList) {
+                    Timestamp timestamp;
+                    boolean timeRelevant = false;
+                    timeRelevant = checkTime(objectToCheck);
+                    System.out.println("time relevant"+timeRelevant);
+                    if (!timeRelevant) {
+                        //Failure failure = new Failure(objectToCheck, "off", new Timestamp(System.currentTimeMillis()));
+                        //Failure failure1 = new Failure(objectToCheck,"off", new Timestamp(System.currentTimeMillis()),null);
+                        //failureService.addFailure(failure1);
+                        //userRepository.save();
+                    }
+                }
 
-            //List<Message> messageList = messageService.getMessages();
-            List<Objects> objectsList = objectService.getObjectByState(true);
-            for (Objects objectToCheck: objectsList) {
-                Timestamp timestamp;
-                boolean timeRelevant = false;
-                timeRelevant = checkTime(objectToCheck);
+
+            } catch (InterruptedException e) {
+                logger.severe("there is an error in thread.sleep during verification " + e.getMessage());
             }
-
-
-        } catch (InterruptedException e) {
-            logger.severe("there is an error in thread.sleep during verification "+e.getMessage());
         }
     }
     public boolean checkTime(Objects objects){
         Timestamp timestampCurrent = new Timestamp(System.currentTimeMillis());
         long longCurrent = timestampCurrent.getTime();
-        /*List<Message> messageList = messageService.getMessages();
-        Message message = messageList.get(messageList.size());
-        Timestamp timestampLastMessage = message.getDateTime();
+        long longLastMessage = 0;
+        long secondsDifference;
 
-        long longLastMessage = timestampLastMessage.getTime();
-        long longCurrent = timestampCurrent.getTime();
-        long secondsDifference = TimeUnit.MILLISECONDS.toSeconds(longCurrent - longLastMessage);
+        switch (objects.getObjectType()){
 
-         */
-        switch (objects.getObjectType()){//TODO make other cases for each connected object
-            /*case "OVEN":
-                long longLastMessage = timestampLastMessage.getTime();
-                long longCurrent = timestampCurrent.getTime();
-                long longDifference = longCurrent - longLastMessage;
-            */
+
+            case "OVEN":
+                List<OvenHistory> ovenHistories = ovenHistoryService.getOvenHistoryByObjectsId(objects.getId());
+                for(OvenHistory ovenHistory : ovenHistories) {                                                      //find more recent message
+                    long longCheckMessage = ovenHistory.getMessageTimestamp().getTime();
+                    longLastMessage = Math.max(longLastMessage, longCheckMessage);
+                }
+                secondsDifference = TimeUnit.MILLISECONDS.toSeconds(longCurrent -longLastMessage);
+                logger.info("difference log = "+ secondsDifference);
+                if (secondsDifference<60000) {
+                    logger.info("time is relevant for " + objects.getObjectType() + " number " + objects.getId());
+                    return true;
+                }break;
+
+
             case "LAMP":
-                //TODO lampHistoryRepository
-                return true;
-                /*if (secondsDifference<6000)
-                    return true;*/
+                List<LampHistory> lampHistories = lampHistoryService.getLampHistoryByObjectsId(objects.getId());
+                for(LampHistory lampHistory : lampHistories) {                                                      //find more recent message
+                    long longCheckMessage = lampHistory.getMessageTimestamp().getTime();
+                    longLastMessage = Math.max(longLastMessage, longCheckMessage);
+                }
+                secondsDifference = TimeUnit.MILLISECONDS.toSeconds(longCurrent -longLastMessage);
+                logger.info("difference log = "+ secondsDifference);
+                if (secondsDifference<60000) {
+                    logger.info("time is relevant for " + objects.getObjectType() + " number " + objects.getId());
+                    return true;
+                }break;
+
+
+            case "COFFEEMACHINE":
+                List<CoffeeMachineHistory> coffeeMachineHistories = coffeeMachineHistoryService.getCoffeeMachineHistoryByObjectsId(objects.getId());
+                for(CoffeeMachineHistory coffeeMachineHistory : coffeeMachineHistories) {                                                      //find more recent message
+                    long longCheckMessage = coffeeMachineHistory.getMessageTimestamp().getTime();
+                    longLastMessage = Math.max(longLastMessage, longCheckMessage);
+                }
+                secondsDifference = TimeUnit.MILLISECONDS.toSeconds(longCurrent -longLastMessage);
+                logger.info("difference log = "+ secondsDifference);
+                if (secondsDifference<60000) {
+                    logger.info("time is relevant for " + objects.getObjectType() + " number " + objects.getId());
+                    return true;
+                }break;
+
+
+            case "SHUTTER":
+                List<ShutterHistory> shutterHistories = shutterHistoryService.getShutterHistoryByObjectsId(objects.getId());
+                for(ShutterHistory shutterHistory : shutterHistories) {                                                      //find more recent message
+                    long longCheckMessage = shutterHistory.getMessageTimestamp().getTime();
+                    longLastMessage = Math.max(longLastMessage, longCheckMessage);
+                }
+                secondsDifference = TimeUnit.MILLISECONDS.toSeconds(longCurrent -longLastMessage);
+                logger.info("difference log = "+ secondsDifference);
+                if (secondsDifference<60000) {
+                    logger.info("time is relevant for " + objects.getObjectType() + " number " + objects.getId());
+                    return true;
+                }break;
+            case "ALARMCLOCK":
+                List<AlarmClockHistory> alarmClockHistories = alarmClockHistoryService.getAlarmClockHistoryByObjectsId(objects.getId());
+                for(AlarmClockHistory alarmClockHistory : alarmClockHistories) {                                                      //find more recent message
+                    long longCheckMessage = alarmClockHistory.getMessageTimestamp().getTime();
+                    longLastMessage = Math.max(longLastMessage, longCheckMessage);
+                }
+                secondsDifference = TimeUnit.MILLISECONDS.toSeconds(longCurrent -longLastMessage);
+                logger.info("difference log = "+ secondsDifference);
+                if (secondsDifference<60000) {
+                    logger.info("time is relevant for " + objects.getObjectType() + " number " + objects.getId());
+                    return true;
+                }break;
             default:
+                logger.info("type is not relevant for "+ objects.getObjectType()+" number "+ objects.getId());
                 return false;
+
         }
+        logger.info("time is not relevant for "+ objects.getObjectType()+" number "+ objects.getId()+". Must register failure.");
+        return false;
+
 
     }
 }
--- src/test/java/com/application/aled/messages/WaitingTimeCheckerTest.java
+++ src/test/java/com/application/aled/messages/WaitingTimeCheckerTest.java
@@ -2,6 +2,9 @@ package com.application.aled.messages;
 
 import com.application.aled.entity.Message;
 import com.application.aled.entity.Objects;
+import com.application.aled.entity.history.LampHistory;
+import com.application.aled.repository.history.LampHistoryRepository;
+import com.application.aled.service.history.LampHistoryService;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,9 +12,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -28,13 +36,35 @@ public class WaitingTimeCheckerTest {
 
     }
 */
-    @Test
+    @Mock
+    LampHistoryService lampHistoryService;
+
+
     public void checkTimeTestLamp(/*Message message, Objects objects*/){
         Objects object = new Objects();
         object.setObjectType("LAMP");
         WaitingTimeChecker waitingTimeChecker = new WaitingTimeChecker();
+        LampHistory lampTest = new LampHistory("COLOR", "RED", new Timestamp(System.currentTimeMillis()),object);
+        List<LampHistory> lampHistoryList = new ArrayList<LampHistory>();
+        lampHistoryList.add(lampTest);
+        Mockito.when(lampHistoryService.getLampHistoryByObjectsId(object.getId())).thenReturn(lampHistoryList);
         Assertions.assertTrue(waitingTimeChecker.checkTime(object));
         //Assertions.assertTrue(true);
     }
 
+    public void checkTimeTestLampNotRelevantDate(){
+        Objects object = new Objects();
+        object.setObjectType("LAMP");
+        WaitingTimeChecker waitingTimeChecker = new WaitingTimeChecker();
+        Date dateNotRelevant = new Date();
+        int hour =dateNotRelevant.getHours()-1;
+        dateNotRelevant.setHours(hour);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        LampHistory lampTest = new LampHistory("COLOR", "RED", timestamp,object);
+        List<LampHistory> lampHistoryList = new ArrayList<LampHistory>();
+        lampHistoryList.add(lampTest);
+        Mockito.when(lampHistoryService.getLampHistoryByObjectsId(object.getId())).thenReturn(lampHistoryList);
+        Assertions.assertFalse(waitingTimeChecker.checkTime(object));
+        //Assertions.assertTrue(true);
+    }
 }
