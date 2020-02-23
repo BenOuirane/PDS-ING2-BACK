3961ffc8be2f3b1cd116d06b12fd43a54e56833b
JAguesse <jaguesse@outlook.fr>
2020-02-23 17:47:01 +0100

    correction on Oven
    no impact

--- src/main/java/com/application/aled/service/OvenServiceImpl.java
+++ src/main/java/com/application/aled/service/OvenServiceImpl.java
@@ -9,18 +9,20 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class OvenServiceImpl implements OvenService {
     @Autowired
     OvenRepository ovenRepository;
 
+    Logger logger = Logger.getLogger("com.application.aled.controller.OvenController");
+
     @Override
     public List<Oven> getOven(Objects objects) {
-        System.out.println("Getting oven for object : " + objects);
+        logger.info("Getting oven for object : " + objects);
         List<Oven> ovens = new ArrayList<>();
         ovenRepository.findAllByObjects(objects).forEach(ovens::add);
-        System.out.println("Getting oven for object 2 : " + ovens);
         return ovens;
     }
 }
