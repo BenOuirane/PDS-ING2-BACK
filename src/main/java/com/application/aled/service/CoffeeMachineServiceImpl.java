d79df66920ff73d094b1cf879585b391bdf97ba3
JAguesse <jaguesse@outlook.fr>
2020-04-02 17:46:08 +0200

    Developement of service + controller for CoffeeMachine object
    + Controller
    + Service
    + save()+ update and implementation of the service

--- src/main/java/com/application/aled/service/CoffeeMachineServiceImpl.java
+++ src/main/java/com/application/aled/service/CoffeeMachineServiceImpl.java
@@ -2,6 +2,7 @@ package com.application.aled.service;
 
 import com.application.aled.entity.CoffeeMachine;
 import com.application.aled.entity.Objects;
+import com.application.aled.entity.Shutter;
 import com.application.aled.repository.CoffeeMachineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,18 @@ public class CoffeeMachineServiceImpl implements CoffeeMachineService {
         List<CoffeeMachine> coffeeMachines = new ArrayList<>();
         coffeeMachineRepository.findAllByObjects(objects).forEach(coffeeMachines::add);
         return coffeeMachines;
+    }
+
+    @Override
+    public boolean updateCoffeeMachine(CoffeeMachine coffeeMachine) {
+        logger.info("Update CoffeeMachine param...");
+        try{
+            coffeeMachineRepository.save(coffeeMachine);
+            return true;
+        }catch (Exception e){
+            logger.info("La machine a café n'a pas été correctement mis à jour...! => Error : service.ShutterServiceImpl");
+            logger.info(e.getMessage());
+            return false;
+        }
     }
 }
