a3abb13c93d5feeecbf45f156c3013b134d1641b
Ismail El Hammoud <ismail16051999@gmail.com>
2020-04-20 09:47:42 +0200

    Adding some methods, and starting I3

--- src/main/java/com/application/aled/repository/CurrentAreaRepository.java
+++ src/main/java/com/application/aled/repository/CurrentAreaRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.application.aled.entity.Bracelet;
 import com.application.aled.entity.CurrentArea;
 
+@Repository
 public interface CurrentAreaRepository extends CrudRepository<CurrentArea, Integer>{
 	
 	//TODO to be deactivated
@@ -17,12 +19,12 @@ public interface CurrentAreaRepository extends CrudRepository<CurrentArea, Integ
 	//public CurrentArea findAreaByBraceletId(Long idBrac);
 
 
-    @Query("select f from current_area f where year(f.cross_date) = ?1")
+    @Query(value = "select a from current_area a where year(a.cross_date) = ?1", nativeQuery = true)
     List<CurrentArea> findAreaByYY(int year);
 
-    @Query("select f from current_area f where year(f.cross_date) = ?1 and month(f.cross_date)= ?2")
+    @Query(value ="select a from current_area a where year(a.cross_date) = ?1 and month(a.cross_date)= ?2", nativeQuery = true)
     List<CurrentArea> findAreaByYYMM(int year, int month);
 
-    @Query("select f from current_area f where year(f.cross_date) = ?1 and month(f.cross_date)= ?2 and day(f.cross_date)= ?3")
+    @Query(value ="select a from current_area a where year(a.cross_date) = ?1 and month(a.cross_date)= ?2 and day(a.cross_date)= ?3", nativeQuery = true)
     List<CurrentArea> findAreaByYYMMDD(int year, int month, int date);
 }
