5fcd1aea15dc21afd4cdbb535b9722e7bf34feb5
BenOuirane <33792892+BenOuirane@users.noreply.github.com>
2020-05-15 14:51:46 +0200

    add score service

---
+++ src/main/java/com/application/aled/service/ScoreService.java
@@ -0,0 +1,14 @@
+package com.application.aled.service;
+/**
+ * Class: ScoreService
+ * @author: BEN OUIRANE Hajer
+ */
+
+import com.application.aled.entity.Score;
+
+import java.util.List;
+
+public interface ScoreService {
+
+    Score add(Score s);
+}
