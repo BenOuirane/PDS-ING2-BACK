8d63b28705e2e5d4b8fe7a7a0da1aa5eabc3271f
BenOuirane <33792892+BenOuirane@users.noreply.github.com>
2020-05-15 14:02:22 +0200

    add candidate controller

--- src/main/java/com/application/aled/controller/CandidateController.java
+++ src/main/java/com/application/aled/controller/CandidateController.java
@@ -1,77 +1,109 @@
+package com.application.aled.controller;
 /**
  * Class: CandidateController
  * @author: BEN OUIRANE Hajer
- * @version: 1.1
+
  */
 
-package com.application.aled.controller;
+
 import com.application.aled.entity.Candidate;
+import com.application.aled.entity.Score;
 import com.application.aled.repository.CandidateRepository;
+import com.application.aled.repository.ScoreRepository;
+import com.application.aled.service.CandidateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
 @RestController
-public class CandidateController {
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CandidateController{
 
 
-    /**
-     * Import the specific repository we need
-     */
+    @Autowired
+    CandidateService candidateService;
+
+    @GetMapping("/candidate")
+    public List<Candidate> tolist() {
+
+        return candidateService.tolist();
+    }
+
+
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private ScoreRepository scoreRepository;
 
-
-    /**
-     * Retrieve All candidates
-     */
 
     @GetMapping("/candidates")
     public List<Candidate> getAllCandidates() {
 
-        System.out.println("Get all Customers...");
+        System.out.println("Get all Candidate...");
 
         List<Candidate> candidates = new ArrayList<>();
         candidateRepository.findAll().forEach(candidates::add);
+
 
         return candidates;
 
     }
 
-    /**
-     * Method that create candidate in our database
-     */
-     Map<String, String> errors;
-    @PostMapping( "/candidates/create")
-    public ResponseEntity<Object> postCandidate(@RequestBody @Valid  Candidate candidate, BindingResult bindingResult) {
 
-        if (bindingResult.hasErrors()){
+    @GetMapping("/score")
+    public List<Candidate> getscoreAllCandidat() {
+
+        return candidateService.getscoreAllCandidat();
+    }
+
+
+    Map<String, String> errors;
+
+    @PostMapping("/candidates/create")
+    public ResponseEntity<Object> postCandidate(@RequestBody @Valid Candidate candidate, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
 
             errors = new HashMap<>();
-            for(FieldError error:bindingResult.getFieldErrors()){
+            for (FieldError error : bindingResult.getFieldErrors()) {
                 errors.put(error.getField(), error.getDefaultMessage());
             }
             return new ResponseEntity<>(errors, HttpStatus.NOT_ACCEPTABLE);
         }
-         Candidate c = candidateRepository.findBymail(candidate.getMail());
-        if(c!=null){
-            return  new ResponseEntity<>(HttpStatus.CONFLICT);
+        Candidate c = candidateRepository.findBymail(candidate.getMail());
+        if (c != null) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
 
-        return new ResponseEntity<>(candidateRepository.save(candidate),HttpStatus.OK);
+        return new ResponseEntity<>(candidateRepository.save(candidate), HttpStatus.OK);
     }
+
+
+
+
+
+    @GetMapping("/sort")
+    public ArrayList<Candidate> sortScoreAllCandidat() {
+
+        return candidateService.sortScoreAllCandidat();
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
